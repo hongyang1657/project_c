f9c66f90008ce4dace5b699f68e9c7c45909d9a1
hongy <hongy@fitme.ai>
2021-06-28 17:04:29 +0800

    init

---
+++ airkiss/wifi/CMakeFiles/net_config.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hongy/C_C++/airkiss/wifi/capture/common.c" "/home/hongy/C_C++/airkiss/wifi/CMakeFiles/net_config.dir/capture/common.c.o"
+  "/home/hongy/C_C++/airkiss/wifi/capture/linux.c" "/home/hongy/C_C++/airkiss/wifi/CMakeFiles/net_config.dir/capture/linux.c.o"
+  "/home/hongy/C_C++/airkiss/wifi/capture/osdep.c" "/home/hongy/C_C++/airkiss/wifi/CMakeFiles/net_config.dir/capture/osdep.c.o"
+  "/home/hongy/C_C++/airkiss/wifi/capture/radiotap/radiotap-parser.c" "/home/hongy/C_C++/airkiss/wifi/CMakeFiles/net_config.dir/capture/radiotap/radiotap-parser.c.o"
+  "/home/hongy/C_C++/airkiss/wifi/src/airkiss_src.c" "/home/hongy/C_C++/airkiss/wifi/CMakeFiles/net_config.dir/src/airkiss_src.c.o"
+  "/home/hongy/C_C++/airkiss/wifi/src/api_net_config.c" "/home/hongy/C_C++/airkiss/wifi/CMakeFiles/net_config.dir/src/api_net_config.c.o"
+  "/home/hongy/C_C++/airkiss/wifi/utils/wifi_scan.c" "/home/hongy/C_C++/airkiss/wifi/CMakeFiles/net_config.dir/utils/wifi_scan.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_AIRKISS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ fd_dp/CMakeFiles/DP.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hongy/C_C++/fd_dp/cJSON.c" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/cJSON.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hongy/C_C++/fd_dp/Tool.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/Tool.cc.o"
+  "/home/hongy/C_C++/fd_dp/events2chars.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/events2chars.cc.o"
+  "/home/hongy/C_C++/fd_dp/fd_config.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/fd_config.cc.o"
+  "/home/hongy/C_C++/fd_dp/fd_dp.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/fd_dp.cc.o"
+  "/home/hongy/C_C++/fd_dp/fd_intent_npu.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/fd_intent_npu.cc.o"
+  "/home/hongy/C_C++/fd_dp/fd_log.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/fd_log.cc.o"
+  "/home/hongy/C_C++/fd_dp/fd_resource.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/fd_resource.cc.o"
+  "/home/hongy/C_C++/fd_dp/fd_slot_npu.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/fd_slot_npu.cc.o"
+  "/home/hongy/C_C++/fd_dp/serialManager.cc" "/home/hongy/C_C++/fd_dp/CMakeFiles/DP.dir/serialManager.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
